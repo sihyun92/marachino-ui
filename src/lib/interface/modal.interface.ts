edb34e1f1a80335c73f9246f2719cda298eb7221
SiHyun <there7788@gmail.com>
2024-12-18 10:47:13 +0900

    Feature: ModalProps 정의
    - width: 모달의 너비 (optional)
    - height: 모달의 길이(optional)
    - backdrop: 뒷 배경 추가 여부 (optional)
    - shadow: 그림자 추가 여부 (optional)

--- src/lib/interface/modal.interface.ts
+++ src/lib/interface/modal.interface.ts
@@ -1,18 +1,8 @@
 import { HTMLAttributes } from "react";
 
-type ModalType = "" | "";
-
 export interface ModalProps extends HTMLAttributes<HTMLDivElement> {
-  zIndex?: string;
-  padding?: string;
   width?: string;
   height?: string;
-  shadow?: boolean;
-  outline?: string;
-  background?: string;
   backdrop?: boolean;
-  position?: boolean;
-  modalType?: ModalType;
-  justifyContent?: string;
-  alignItems?: string;
+  shadow?: boolean;
 }
