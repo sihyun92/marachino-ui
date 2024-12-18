4681d147bc8c30f8a6ae341a6f83c5b84373c03e
SiHyun <there7788@gmail.com>
2024-12-18 10:50:01 +0900

    Fix: width, height에 있는 optional chaining 제거

--- src/lib/interface/modal.interface.ts
+++ src/lib/interface/modal.interface.ts
@@ -1,8 +1,8 @@
 import { HTMLAttributes } from "react";
 
 export interface ModalProps extends HTMLAttributes<HTMLDivElement> {
-  width?: string;
-  height?: string;
+  width: string;
+  height: string;
   backdrop?: boolean;
   shadow?: boolean;
 }
